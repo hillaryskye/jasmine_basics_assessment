321f9c3a13238b39c9c67a10e0c878a12c03b3cd
Hillary Skye <skyeinc@qwestoffice.net>
2015-06-04 12:48:47 -0600

    Finished lettergrader

--- grader.js
+++ grader.js
@@ -16,5 +16,40 @@ letterGrader: function(grade) {
          if (grade >= 0 && grade <= 59) {
              return 'F';
          }
-       }
+       },
+
+getAverage: function(array) {
+             sum = 0;
+               array.forEach(function(element) {
+                   sum += element;
+           });
+           return sum/array.length;
+       },
+
+medianScore: function(values) {
+    values.sort( function(a,b) {return a - b;} );
+
+    var half = Math.floor(values.length / 2); // gets the middle point in the array
+
+    if(values.length % 2){ // figures out if the value is in the middle
+      return values[half];
+    }
+    else{                  // because value is an even num, it calculates the aver of the middle 2 nums
+      return (values[half - 1] + values[half]) / 2.0; // Calculates the average of the middle 2 nums
+    }
+  },
+  modeScore: function mode(arr) {
+    var numMapping = {};
+    var greatestFreq = 0;
+    var mode;
+    arr.forEach(function findMode(number) {
+        numMapping[number] = (numMapping[number] || 0) + 1; // Places the array indexes in an object
+                                                            // adds the next index into the object
+        if (greatestFreq < numMapping[number]) {
+            greatestFreq = numMapping[number];
+            mode =+ number; // Returns the frequency of the array item
+        }
+    });
+    return mode;
+    }
 };
--- spec/spec.js
+++ spec/spec.js
@@ -2,10 +2,10 @@ var myCode = require('./../grader.js');
 
 describe('A function which takes a test score and returns the equivalent letter grade.', function() {
    it('#letterGrader()', function() {
-     expect(myCode.letterGrader(98)).toEqual('A');
+     expect(myCode.letterGrader(92)).toEqual('A');
    });
    it('#letterGrader()', function() {
-     expect(myCode.letterGrader(82)).toEqual('B');
+     expect(myCode.letterGrader(87)).toEqual('B');
    });
    it('#letterGrader()', function() {
      expect(myCode.letterGrader(73)).toEqual('C');
@@ -43,15 +43,27 @@ describe('A function which takes a test score and returns the equivalent letter 
    it('#letterGrader()', function() {
      expect(myCode.letterGrader('%' || '$' || '@' || '&' || '*' || '(' || ')' || '!')).not.toEqual('A');
    });
+ });
 
+   describe('A function which takes an array of test scores and returns the average score.', function() {
+     it('#getAverage()', function() {
+     expect(myCode.getAverage([90, 95, 87, 60])).toEqual(83);
+   });
+ });
 
+   describe('A function which takes an array of test scores and calculates the median score.', function() {
+       it('#medianScore()', function() {
+         expect(myCode.medianScore([52,80,80,86,94])).toEqual(80);
+         expect(myCode.medianScore([73, 51, 36, 92, 1, 86])).toEqual(62);
+         expect(myCode.medianScore(['a', 'b', 'c', 'c', '0'])).not.toEqual(92);
+         expect(myCode.modeScore([8210, -1, null, 'sdf'])).not.toEqual(92);
+   });
+ });
 
-
-
-
-
-
-
-
-
+   describe('A function `modeScore` which takes an array of test scores and calculates the mode score.', function() {
+     it('#modeScore()', function() {
+       expect(myCode.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
+       expect(myCode.modeScore(['a', 'b', 'c', 'a', 'a', 'd', 'e'])).not.toEqual(92);
+       expect(myCode.modeScore([8210, -1, null, 'sdf'])).not.toEqual(92);
+     });
 });
